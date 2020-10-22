f193b64968d2eacc835dfe2923a2a213747690d5
Katarzyna <kataszl768@student.polsl.pl>
2020-10-22 12:40:43 +0200

    struct 2D cd

--- lab3/2.cpp
+++ lab3/2.cpp
@@ -9,18 +9,40 @@ struct Point2D
     double y;
 };
 
+Point2D create_point()
+{
+    Point2D tmp;
+    tmp.x=rand()%100;
+    tmp.y=rand()%100;
+
+    return tmp;
+}
+
+void initialize_in_place(Point2D& point)
+{
+    point.x=rand()%10;
+    point.y=rand()%10;
+
+    cout<<"the value of x: "<<point.x<<endl;
+    cout<<"the value of y: "<<point.y<<endl;
+}
+
 int main()
 {
     //declare
-    Point2D mypoint;
+    Point2D my_point=create_point();
 
     //initialize
-    mypoint.x=rand()%100;
-    mypoint.y=rand()%100;
+    cout<<"the value of x: "<<my_point.x<<endl;
+    cout<<"the value of y: "<<my_point.y<<endl;
+
+    //declare
+    Point2D empty_point;
+    initialize_in_place(empty_point);
 
     //print
-    cout<<"the value of x: "<<mypoint.x<<endl;
-    cout<<"the value of y: "<<mypoint.y<<endl;
+    cout<<"the value of x: "<<empty_point.x<<endl;
+    cout<<"the value of y: "<<empty_point.y<<endl;
 
     return 0;
 }

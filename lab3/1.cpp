cfe1c5ac0b1ec98600cb331b87529a9872211382
Katarzyna <kataszl768@student.polsl.pl>
2020-10-22 12:12:31 +0200

    bubble sorter

--- lab3/1.cpp
+++ lab3/1.cpp
@@ -7,7 +7,9 @@ int main()
 {
     array<int,10> numbers={6,2,8,5,1,3,7,4};
 
-    for(int i=0;i<10;i++)
+    for (int j=8;j>0;j--)
+    {
+    for(int i=0;i<j;i++)
     {
         if (numbers[i]>numbers[i+1])
         {
@@ -15,6 +17,13 @@ int main()
             numbers[i]=numbers[i+1];
             numbers[i+1]=left;
         }
+
+
+    }}
+
+    for (int i=0;i<8;i++)
+    {
+        cout<<numbers[i]<<endl;
     }
 
     return 0;

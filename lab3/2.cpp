caad499cda5b4dd24c0552dab2df5adae9728d73
Katarzyna <kataszl768@student.polsl.pl>
2020-10-22 12:22:50 +0200

    struct 2D

--- lab3/2.cpp
+++ lab3/2.cpp
@@ -3,7 +3,20 @@
 
 using namespace std;
 
+struct Point2D
+{
+    double x;
+    double y;
+};
+
 int main()
 {
+    Point2D mypoint;
+
+    mypoint.x=3.3;
+    mypoint.y=5.5;
+
+    cout<<"the value of x: "<<mypoint.x<<endl;
+    cout<<"the value of y: "<<mypoint.y<<endl;
 
     return 0;}

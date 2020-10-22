fb30a4de241490a26033bc79f7a922444e94bb2e
Katarzyna <kataszl768@student.polsl.pl>
2020-10-22 13:12:19 +0200

    pointsy

--- lab3/3.cpp
+++ lab3/3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <cmath>
 
 using namespace std;
 
@@ -18,14 +19,44 @@ Point2D create_point()
     return tmp;
 }
 
+float distance(Point2D left,Point2D right)
+{
+    float res_x=pow((right.x-left.x),2);
+    float res_y=pow((right.y-left.y),2);
+
+    return sqrt(res_x+res_y);
+}
+
 int main()
 {
     array<Point2D,10>points;
+
     for (int i=0;i<10;i++)
     {
         points[i]=create_point();
         cout<<"point["<<i<<"]=("<<points[i].x<<","<<points[i].y<<")"<<endl;
     }
+
+    float largest_distance=0.0;
+    Point2D first, second;
+
+    for (int i=0;i<10;i++)
+    {
+        for (int j=0;j<10;j++)
+        {
+            float d=distance(points[i],points[j]);
+            if (d>=largest_distance)
+            {
+                largest_distance=d;
+                first=points[i];
+                second=points[j];
+            }
+        }
+    }
+
+    cout<<"the largest distance is: "<<largest_distance<<endl;
+    cout<<"first point: ("<<first.x<<","<<first.y<<")"<<endl;
+    cout<<"second point: ("<<second.x<<","<<second.y<<")"<<endl;
 
 
     return 0;
